7ad254c3b3a36745eb2e12f87f5e0f8632ac53d1
boygl34 <56708291+boygl34@users.noreply.github.com>
2023-05-08 14:46:42 +0700

    add

--- main.js
+++ main.js
@@ -12,27 +12,38 @@ const adapter = new FileSync('db.json')
 const db = low(adapter)
 
 
-
-console.log('ád', db.get('Login').value());
-
-// const io = new io.Server(server, {
-//     cors: { origin: '*' }
-// });
-
-//const { Login, XeTrongXuong, XeDaGiao } = db.data
 io.on('connection', async (socket) => {
+ 
+console.log('Client connected', socket.id);
 
-    console.log('Client connected', socket.id);
     socket.on('login', async (data) => {
-        let user = { name: data.name, id: socket.id }
-        db.get('Login')
-            .push(user)
-            .write()
-        socket.emit("sendData", db.get('XeTrongXuong').value())
+            let user = { name: data.name, id: socket.id }
+            db.get('Login').push(user).write()
+            io.to(`${ socket.id}`).emit('login', socket.id);     
+            io.to(`${ socket.id}`).emit('sendData', db.get('XeTrongXuong').value());      
     });
     socket.on("disconnect", async () => {
+        try{
+            db.get('Login').remove({ id: socket.id }).write()
+            console.log('Client disconnect', socket.id);}
+        catch(error){
+            console.log("Không tìm thấy ID")
+        }
+    });
 
-        console.log('Client disconnect', socket.id);
+    socket.on("dangky", async (data) => {
+        try{
+            const newId = getLastId(data.path) + 1
+            let  User = await db.get('Login').find({ id: socket.id }).value();
+            if(!data.data.id){data.data.id = newId}
+            data.data.LastUser = User.name
+            await db.get(data.path).push(data.data).write()
+            io.emit('sendData', db.get(data.path).value()); 
+        }
+        catch(error){
+            console.log(error);
+            io.to(`${ socket.id}`).emit('error', {message:"Không Thể Đăng Ký"}); 
+        }
     });
 
 
@@ -47,3 +58,17 @@ server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 
 });
+
+
+  function  getLastId(path) {
+    if (  db.has(path).value()) {
+        let sorted =  db.get(path)
+            .value()
+            .sort( (a, b) => {
+                return b.id - a.id;
+            });
+        
+        return sorted[0] ? sorted[0].id : 1;
+    }
+    return 1;
+} 
